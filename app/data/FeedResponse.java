47946b209f09e7755acc02fdafd9d594d2a38a8e
stagefright5 <sankarshanaj@gmail.com>
2017-10-07 12:32:55 +0530

    tooMuchDone

--- app/data/FeedResponse.java
+++ app/data/FeedResponse.java
@@ -2,6 +2,7 @@ package data;
 
 public class FeedResponse {
     public String title;
-    public String body;
+    public String description;
     public String pubDate;
+
 }
--- app/data/Message.java
+++ app/data/Message.java
@@ -3,7 +3,10 @@ package data;
 public class Message {
     public String text;
     public String time;
-    public enum Sender{BOT,USER};
+    public enum Sender{
+        BOT,USER
+    };
     public Sender sender;
+    public FeedResponse feedResponse;
 
 }
