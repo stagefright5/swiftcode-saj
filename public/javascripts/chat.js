5deed1c2348686f3f7821d1823a6d66ec240d494
stagefright5 <sankarshanaj@gmail.com>
2017-10-08 10:56:41 +0530

    v3.1-message bubble created and styled

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,20 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+        $scope.messages=[
+        {
+            'sender':'USER',
+            'text':'hello',
+            'time':'10:01 AM'
+        },
+        {
+            'sender':'BOT',
+            'text':'What news do you want?',
+            'time':'10:01 AM'
+        },
+        {
+            'sender':'USER',
+            'text':'news about nothing',
+            'time':'10:02 AM'
+        }]
 });
