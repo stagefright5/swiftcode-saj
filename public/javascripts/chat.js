b0bc7f5638676d82adae8491c203beab877c2891
stagefright5 <sankarshanaj@gmail.com>
2017-10-08 16:11:31 +0530

    3.3-Final(Dark Commented)

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,19 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
+app.config(function ($mdThemingProvider) {
+   $mdThemingProvider.theme('default')
+       .primaryPalette('grey', {
+                                 'default':'400', // by default use shade 400 from the pink palette for primary intentions
+                                 'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+                                 'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+                                 'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+                               })
+        .accentPalette('grey',{
+                                'default':'900'
+        })
+        //.dark();
+});
+
 app.controller('chatController', function ($scope, $sce) {
         $scope.messages=[];
         var  exampleSocket =  new  WebSocket('ws://localhost:9000/chatSocket');
